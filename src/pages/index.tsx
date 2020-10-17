9dfb921eaee954f79b27f8285e31cc56ea5bb8b6
Bruno Cruz <bru.cruz@gmail.com>
2020-10-17 16:03:06 -0300

    Tried to config eslint and prettier, but it does not seem to work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+
 export default function Home() {
+  const t = 'ok'
+
+  useEffect(() => {
+    console.log(t);
+  }, [])
+
+
   return (
     <div>
       <h1>Hello Sagittarius</h1>
