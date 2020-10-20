ec120a577525d84bb91bb886079777b20a7532ee
Bruno Cruz <bru.cruz@gmail.com>
2020-10-20 11:28:41 -0300

    Minor changes eliminating some commented code

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 // next.config.js
 const withImages = require('next-images')
 module.exports = withImages()
-
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"]
-//     });
-
-//     return config;
-//   }
-// };
--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,13 +1,3 @@
-// declare module "*.svg" {
-//   const content: any;
-//   export default content;
-// }
-
-// declare module "*.png" {
-//   const content: any;
-//   export default content;
-// }
-
 declare module '*.png'
 declare module '*.jpg'
 declare module '*.jpeg'
