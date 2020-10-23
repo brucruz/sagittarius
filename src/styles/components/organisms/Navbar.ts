576f0546d92df68d749c0bcaa22bb1fc2f63c212
Bruno Cruz <bru.cruz@gmail.com>
2020-10-23 02:25:26 -0300

    fix: css on home page

--- src/styles/components/organisms/Navbar.ts
+++ src/styles/components/organisms/Navbar.ts
@@ -5,11 +5,14 @@ export const Container = styled.header`
   z-index: 1000;
   position: fixed;
   margin: 0;
-  padding: 0 8.3%;
   top: 0;
   width: 100%;
   background: #FFFFFF;
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.08);
+
+  @media ${device.laptop} {
+    padding: 0 8.3%;
+  }
 `;
 
 export const Content = styled.section`
@@ -35,7 +38,7 @@ export const Logo = styled.h1`
   margin-bottom: 8px;
   margin-left: 16px;
 
-  @media ${device.tablet} {
+  @media ${device.laptop} {
     font-size: 30px;
     line-height: 41px;
     margin-left: 0;
@@ -66,7 +69,11 @@ export const NavLinks = styled.section`
     color: #232B3B;
 
     & + p {
-      margin-left: 49px;
+      margin-left: 1.5rem;
+
+      @media ${device.laptopL} {
+        margin-left: 49px;
+      }
     }
   }
 `;
