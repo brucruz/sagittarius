61853b98fc90ba18ab71e29cb664029a81e0a947
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-10-30 17:16:35 -0300

    feat: nps modal

--- src/pages/checkout/[patientId]/obrigado.tsx
+++ src/pages/checkout/[patientId]/obrigado.tsx
@@ -1,9 +1,80 @@
+import { useState } from 'react';
 import PageTemplate from '@/components/templates/PageTemplate';
-import { Smile, Title, Subtitle, Content } from '@/styles/pages/checkout/[patientId]/ThankYou';
-import ButtonNext from '@/components/atom/ButtonNext';
+import { Smile, Title, Subtitle, Content, ModalContainer } from '@/styles/pages/checkout/[patientId]/ThankYou';
+import Button from '@/components/atom/Button';
 import Link from 'next/link';
+import { MdClose } from 'react-icons/md';
+import Modal from '@/components/organisms/Modal';
+
+import starUnfilled from '@/assets/pages/ThankYou/star-unfilled.svg';
+import starFilled from '@/assets/pages/ThankYou/star-filled.svg';
+import { useAuth } from '@/hooks/auth';
+
+interface star {
+  id: number,
+  isFilled: boolean
+}
 
 export default function ThankYou() {
+
+  const { user } = useAuth();
+
+  const [rateStars, setRateStart] = useState<star[]>([
+    {
+      id: 1,
+      isFilled: false
+    },
+    {
+      id: 2,
+      isFilled: false
+    },
+    {
+      id: 3,
+      isFilled: false
+    },
+    {
+      id: 4,
+      isFilled: false
+    },
+    {
+      id: 5,
+      isFilled: false
+    },{
+      id: 6,
+      isFilled: false
+    },{
+      id: 7,
+      isFilled: false
+    },{
+      id: 8,
+      isFilled: false
+    },
+    {
+      id: 9,
+      isFilled: false
+    },
+    {
+      id: 10,
+      isFilled: false
+    },
+  ]);
+
+  const [userRate, setUserRate] = useState(0);
+  const [displayRateModal, setDisplayRateModal] = useState(false);
+
+  function handleChangeStar(starId: number) {
+
+    const starIndex = rateStars.findIndex(star => star.id === starId);
+
+    setRateStart(rateStars.map((star, index) => index <= starIndex ? {
+      id: star.id,
+      isFilled: true
+    } : {
+      id: star.id,
+      isFilled: false
+    }));
+  }
+
   return (
     <PageTemplate
       buttonType={{
@@ -17,10 +88,50 @@ export default function ThankYou() {
           Solicitação de Agendamento!
         </Title>
         <Subtitle>Em instantes você receberá as orientações <br />em seu E-mail e WhatsApp.</Subtitle>
+        <Button onClick={() => setDisplayRateModal(true)}>Avaliar experiência</Button>
         <Link href='/'>
-          <ButtonNext text="Continuar navegando" />
+          <a>Ir para o início</a>
         </Link>
       </Content>
+      <Modal isOpen={displayRateModal} setIsOpen={() => setDisplayRateModal(false)}>
+        <ModalContainer>
+          <div>
+            <MdClose onClick={() => setDisplayRateModal(false)} />
+          </div>
+          <div>
+            <h2>O que está achando da Heali?</h2>
+            <p>Nós valorizamos sua opinião e esperamos que você possa dedicar um minuto para responder algumas perguntas curtinhas que vão nos ajudar a entender como estamos indo.</p>
+            <h3>Qual a probabilidade de você recomendar os serviços da Heali alguém da sua família ou amigo?</h3>
+            <div className="stars-div">
+              {rateStars.map((star: star) => {
+                return (
+                  <div key={star.id}>
+                    <img 
+                      onClick={() => {
+                        setUserRate(star.id);
+                        handleChangeStar(star.id);
+                      }}
+                      src={star.isFilled ? starFilled : starUnfilled} 
+                      alt="Ícone de estrela"
+                    />
+                    {star.id}
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+          <div>
+            <Button type="button">
+              <a 
+                href={`https://airtable.com/shrGy6kCQJwvozf2T?prefill_Name=${user?.first_name}%20${user?.last_name}&prefill_Email=${user?.email}&prefill_NPS Score=${userRate}`}
+                target="blank"
+              >
+                Enviar avaliação
+              </a>
+            </Button>
+          </div>
+        </ModalContainer>
+      </Modal>
     </PageTemplate>
   );
 }
--- src/styles/pages/checkout/[patientId]/ThankYou.ts
+++ src/styles/pages/checkout/[patientId]/ThankYou.ts
@@ -1,3 +1,4 @@
+import device from '@/utils/devices';
 import styled from 'styled-components';
 
 export const Content = styled.div`
@@ -8,6 +9,13 @@ export const Content = styled.div`
 
   button {
     width: 75%;
+    margin-bottom: 16px;
+    text-transform: uppercase;
+  }
+
+  a {
+    text-decoration: none;
+    color: #4D49C4;
   }
 `;  
 
@@ -33,4 +41,109 @@ export const Subtitle = styled.h3`
   letter-spacing: 0.192941px;
   color: #3C4759;
   margin-bottom: 32px;
+`;
+
+export const ModalContainer = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+
+  & > div:first-child {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    padding: 16px;
+
+    svg {
+      fill: #BCC3D4;
+      height: 24px;
+      width: 24px;
+    }
+  }
+
+  & > div:nth-child(2) {
+    padding: 24px;
+
+    p {
+      margin: 32px 0;
+      font-size: 12px;
+    }
+
+    h2 {
+      font-size: 24px;
+    }
+
+    h3 {
+      font-size: 16px;
+    }
+    
+    @media ${device.tablet} {
+      padding: 48px !important;
+
+      h2 {
+        font-size: 32px;
+        text-align: center;
+      }
+
+      p {
+        font-size: 16px;
+      }
+
+      h3 {
+        font-size: 20px;
+        text-align: center;
+      }
+    }
+  }
+
+  & > div:last-child {
+
+    display: flex;
+    justify-content: center;
+    padding: 24px 0;
+    border-top: 1px solid #BCC3D4;
+
+    button {
+      width: 75%;
+      text-transform: uppercase;
+
+      @media ${device.tablet} {
+        width: 50%;
+      }
+
+      a {
+        color: #fff;
+        text-decoration: none;
+      }
+    }
+  }
+
+  .stars-div {
+    margin-top: 16px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      margin: 8px;
+      font-size: 12px;
+
+      img {
+        height: 16px;
+        width: 16px;
+      }
+
+      @media ${device.tablet} {
+
+        font-size: 16px;
+
+        img {
+          height: 24px;
+          width: 24px;
+        }
+      }
+    }
+  }
 `;
