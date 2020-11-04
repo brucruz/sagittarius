43c70cdc0bbc966c1c900b1b348905165da49f76
Bruno Cruz <bru.cruz@gmail.com>
2020-11-04 10:27:58 -0300

    changed _doc script order

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,21 +38,6 @@ export default class MyDocument extends Document {
 
           <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700;800;900&family=Russo+One&display=swap" rel="stylesheet"></link>
 
-          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-
           {/* Google Tag Manager */}
           <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -77,6 +62,22 @@ export default class MyDocument extends Document {
           `
             }}
           />
+
+
+          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
 
         <body>
