4447f017877e9f9a40cfaf6a87e11f63decd6ec1
Bruno Cruz <bru.cruz@gmail.com>
2020-11-10 20:01:36 -0300

    hotfix: fixed sitemap script

--- src/scripts/gsitemap.ts
+++ src/scripts/gsitemap.ts
@@ -1,14 +1,20 @@
 // const Exam = require('@/@types/Exam');
 // const OriginalExam = require('@/@types/OriginalExam');
-const Axios = require('axios');
 const fs = require('fs');
+
+const Axios = require('axios');
 
 const globby = require('globby');
 const prettier = require('prettier');
 const seoLocations = require('../contents/seoLocations');
 
 (async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+  const writeStream = fs.createWriteStream('./public/sitemap.xml');
+
+  writeStream.write(`
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  `);
 
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
@@ -16,6 +22,17 @@ const seoLocations = require('../contents/seoLocations');
     '!pages/_*.js',
     '!pages/api',
   ]);
+
+  const indexUrl = `
+    <url>
+      <loc>${`https://heali.me`}</loc>
+      <lastmod>${new Date()}</lastmod>,
+      <changefreq>weekly</changefreq>
+    </url>
+  `;
+
+  writeStream.write(indexUrl);
+  console.log('Wrote index page');
 
   // Create site entries for lab results
   // Set lab results change frequency as daily
@@ -28,6 +45,26 @@ const seoLocations = require('../contents/seoLocations');
 
   console.log('Fetched location data');
 
+  const resultsUrls = exams
+    .map(exam => {
+      const parsedLocations = locations
+        .map(location => {
+          return `
+        <url>
+          <loc>${`https://heali.me/resultado/?slg[]=${exam.slug}&add=${location.add}&lat=${location.lat}&lng=${location.lng}`}</loc>
+          <lastmod>${exam.updated_date}</lastmod>,
+          <changefreq>daily</changefreq>
+        </url>
+      `;
+        })
+        .join('');
+      return parsedLocations;
+    })
+    .join('');
+
+  writeStream.write(resultsUrls);
+  console.log('Wrote results page');
+
   // Create site entries for lab details
   // Set lab results change frequency as daily
   // const originalExams = await fetchAssociatedOriginalExams();
@@ -37,56 +74,37 @@ const seoLocations = require('../contents/seoLocations');
 
   console.log('Fetched original exams data');
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map(page => {
-                const path = page
-                  .replace('pages', '')
-                  .replace('.js', '')
-                  .replace('.mdx', '');
-                const route = path === '/index' ? '' : path;
-
-                return `
-                        <url>
-                          <loc>${`https://heali.me${route}`}</loc>
-                          <lastmod>${process.env.siteUpdatedAt}</lastmod>,
-                          <changefreq>weekly</changefreq>
-                        </url>
-                    `;
-              })
-              .join('')}
-
-            ${exams.map(exam => {
-              locations.map(location => {
-                return `
-                  <url>
-                    <loc>${`https://heali.me/resultado/?slg[]=${exam.slug}&add=${location.add}&lat=${location.lat}&lng=${location.lng}`}</loc>
-                    <lastmod>${process.env.siteUpdatedAt}</lastmod>,
-                    <changefreq>weekly</changefreq>
-                  </url>
-                `;
-              });
-            })}
-
-            ${originalExams.map(originalExam => {
-              return `
-                <url>
-                  <loc>${`https://heali.me/${originalExam.lab.slug}/?slg[]=${originalExam.exam.slug}`}</loc>
-                  <lastmod>${process.env.siteUpdatedAt}</lastmod>,
-                  <changefreq>weekly</changefreq>
-                </url>
-              `;
-            })}
-        </urlset>
+  const detailsUrls = originalExams
+    .map(originalExam => {
+      return `
+      <url>
+        <loc>${`https://heali.me/${originalExam.lab.slug}/?slg[]=${originalExam.exam.slug}`}</loc>
+        <lastmod>${originalExam.updated_date}</lastmod>,
+        <changefreq>daily</changefreq>
+      </url>
     `;
+    })
+    .join('');
 
-  // If you're not using Prettier, you can remove this.
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: 'html',
-  });
+  writeStream.write(detailsUrls);
+  console.log('Wrote details page');
 
-  fs.writeFileSync('public/sitemap.xml', formatted);
+  writeStream.write(`</urlset>`);
+
+  // const sitemap = `
+  //       <?xml version="1.0" encoding="UTF-8"?>
+  //       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  //         ${pagesUrls}
+  //         ${resultsUrls}
+  //         ${detailsUrls}
+  //       </urlset>
+  //   `;
+
+  // // If you're not using Prettier, you can remove this.
+  // const formatted = prettier.format(sitemap, {
+  //   ...prettierConfig,
+  //   parser: 'html',
+  // });
+
+  // await fs.writeFileSync('public/sitemap.xml', formatted);
 })();
